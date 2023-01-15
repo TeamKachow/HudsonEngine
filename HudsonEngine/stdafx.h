5b0b005afefaf2786d414cc296d3f6498a99fab4
Mathew Dacosta <72082814+mathewdacosta@users.noreply.github.com>
2023-01-15 00:00:32 +0000

    Couple more cleanups

---
+++ HudsonEngine/stdafx.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+// IMPORTANT: Put all your "library" includes here, and then include them from here.
+// This includes C/C++ standard libraries, and any dependencies we're including (imgui/glad/glfw/boost/cereal)
+
+
